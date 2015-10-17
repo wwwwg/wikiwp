c4222db44d61b1a0b139f867212ee9fef71e8e8d
Florian Steller <florian-steller@outlook.de>
2015-10-17 10:48:17 +0200

    removed last posts from wiki page template

--- wiki-page.php
+++ wiki-page.php
@@ -15,27 +15,15 @@ get_template_part('navigation');
 while ( have_posts() ) : the_post();
 ?>
 
-<div class="pageContainer pageContainerWiki">
+<div class="pageContainer">
     <?php
     // get content format
     get_template_part( 'content', get_post_format() );
-
 
     // comments
     comments_template();
     endwhile;
     ?>
-
-    <div class="last-posts-list postinfo clearfix">
-        <hr>
-        <h4 class="lastPostsListTitle">
-            <?php echo __('Last posts', 'wikiwp'); ?>
-        </h4>
-
-        <ul>
-            <?php wp_get_archives('type=postbypost&limit=10'); ?>
-        </ul>
-    </div>
 </div>
 
 <?php
