c74a3b371a3a9b9af1953c51fb4d3a2c7a525e09
Florian Steller <florian-steller@outlook.de>
2015-10-17 10:26:26 +0200

    updated wiki-page header

--- wiki-page.php
+++ wiki-page.php
@@ -1,27 +1,31 @@
 <?php
-    /*
-    Template Name: Wiki
-    */
+/**
+*Template Name: Wiki
+*
+* @package WordPress
+* @subpackage WikiWP
+* @since WikiWP 1.8
+*/
 
-	get_header();
-	get_template_part('navigation');
+get_header();
+get_template_part('navigation');
 
-    // postmeta
-	get_template_part('postmeta' );
-	// POST
-	while ( have_posts() ) : the_post();
-	echo '<div class="content"';
-	post_class('post');
-	echo '>',
-		 '<h1 class="page-title">';
-	the_title();
-	echo '</h1>';
-	the_content();
-	get_template_part('postinfo' );
-    get_sidebar();
-	// comments
-	comments_template();
-	endwhile;
-	echo '</div>'; // end of .content
-    // footer
-    get_footer();
+// postmeta
+get_template_part('postmeta' );
+// POST
+while ( have_posts() ) : the_post();
+echo '<div class="content"';
+post_class('post');
+echo '>',
+	 '<h1 class="page-title">';
+the_title();
+echo '</h1>';
+the_content();
+get_template_part('postinfo' );
+get_sidebar();
+// comments
+comments_template();
+endwhile;
+echo '</div>'; // end of .content
+// footer
+get_footer();
