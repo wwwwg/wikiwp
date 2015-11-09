2e071ba593bee1df028e0bf56a02cbed14a164dd
Florian Steller <florian.steller@wmdb.de>
2015-11-09 14:37:13 +0100

    fixed #23

--- search.php
+++ search.php
@@ -1,51 +1,47 @@
-<?php 
-    get_header(); 
+<?php
+    get_header();
     get_template_part('navigation');
-    // content
-    echo '<div class="content">',
-         '<div class="search-content">',
-         '<h1 class="cat-title">'.$wp_query->found_posts.'&nbsp;';
-    printf( __( 'results found for %s', 'wikiwp' ), '<strong>' . get_search_query() . '</strong>' );
-    echo '</h1>';
-    $posts = query_posts($query_string . '&orderby=name&order=asc&posts_per_page=-1'); 
-    
-    // Post excerpt
-    if ( have_posts() ) : while (have_posts()) : the_post();
-    echo '<div class="excerpt clearfix">';
-         
-    // Thumbnail
-    if ( has_post_thumbnail() ) {
-    echo '<a class="excerpt-thumbnail" href="';
-    the_permalink();
-    echo '" title="';
-    the_title_attribute();
-    echo '">';
-    the_post_thumbnail('mini');
-    echo '</a>';
-    // Post title
-        echo '<h2 class="excerpt-title"><a href="'.get_permalink().'">'.get_the_title().'</a></h2>',
-        // Post info
-             '<div class="postinfo postinfo-excerpt">',
-			 '<span>'.get_the_date().'</span>',
-		   	 '</div>'; // End of .postinfo-excerpt
-    } else {
-        // Post title
-        echo '<h2 class="excerpt-title"><a href="'.get_permalink().'">'.get_the_title().'</a></h2>',
-        // Post info
-             '<div class="postinfo postinfo-excerpt">',
-			 '<span>'.get_the_date().'</span>',
-		   	 '</div>'; // End of .postinfo-excerpt
-    }
-    // Excerpt
-    the_excerpt();
-    // Post info
-    get_template_part('postinfo');
-    echo '</div>'; // End of .excerpt
-    endwhile;
-    echo '</div>'; // End of .search-content
-    endif;
-    echo '</div>'; // End of .content
-    // Sidebar
-    get_sidebar();
-    // Footer
-    get_footer();
+?>
+
+    <div class="searchContainer">
+        <section class="entryTypePostExcerptHeader">
+            <header class="entryHeader">
+                <h1 class="entryTitle">
+                    <?php
+                    echo $wp_query->found_posts.'&nbsp;';
+                    printf( __( 'results found for %s', 'wikiwp' ), '<strong>' . get_search_query() . '</strong>' );
+                    ?>
+                </h1>
+            </header>
+        </section>
+
+        <section class="entryTypePostExcerptContainer">
+            <?php
+            // query for post order
+            $posts = query_posts($query_string . '&orderby=name&order=asc&posts_per_page=-1');
+
+            if ( have_posts() ) : while (have_posts()) : the_post();
+                // get post excerpt
+                wikiwp_get_post_excerpt($post);
+            endwhile;
+            ?>
+        </section>
+
+        <section class="entryTypePostExcerptMeta">
+            <?php
+            // Pagination
+            echo '<div class="posts-pagination">';
+            previous_posts_link('<span class="next-posts-link">&laquo; '.__('Newer Entries', 'wikiwp').'</span>');
+            next_posts_link('<span class="previous-posts-link">'.__('Older Entries', 'wikiwp').' &raquo;</span>');
+            echo '</div>'; // End of .posts-pagination
+            // If no posts were found
+            endif; ?>
+        </section>
+    </div>
+
+<?php
+// sidebar
+get_sidebar();
+
+// footer
+get_footer();
