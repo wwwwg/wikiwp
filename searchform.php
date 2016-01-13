e1d2720e6d78990083657df8b9e6cfcc40e33e46
Florian Steller <florian-steller@outlook.de>
2016-01-13 17:53:51 +0100

    fixed #50

--- searchform.php
+++ searchform.php
@@ -1,8 +1,8 @@
 <?php
 	echo '<form role="search" method="get" class="search-form" action="'.home_url( '/' ).'">',
 		 '<label>',
-		 '<span class="screen-reader-text">'.__( 'Search for:', 'label' ).'</span>',
-		 '<input type="search" class="search-field" placeholder="'.__( 'Search', 'wikiwp' ).'" value="'.get_search_query().'" name="s" title="'.__( 'Search for:', 'wikiwp' ).'" />',
+		 '<span class="screen-reader-text">'.__( 'Search', 'wikiwp' ).'</span>',
+		 '<input type="search" class="search-field" placeholder="'.__( 'Search', 'wikiwp' ).'" value="'.get_search_query().'" name="s" title="'.__( 'Search', 'wikiwp' ).'" />',
 		 '</label>',
-		 '<input type="submit" class="search-submit" value="'.__( 'Search', 'wikiwp' ).'" />',
+		 '<input type="subm*---it" class="search-submit" value="'.__( 'Search', 'wikiwp' ).'" />',
 		 '</form>';
